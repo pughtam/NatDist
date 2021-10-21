9726a01785e0bb6c343483afb7ef3cc544e7e956
pughtam <thomas.pugh@nateko.lu.se>
2021-10-21 20:44:30 +0200

    Add additional output files to the netcdf comversion scripts.

--- lpjg_processing/lpjg_to_netcdf.m
+++ lpjg_processing/lpjg_to_netcdf.m
@@ -22,6 +22,7 @@ dreal={'standard','low','high',...
 nsims=length(sims);
 
 PFTs={'BNE','BINE','BNS','TeNE','TeBS','IBS','TeBE','TrBE','TrIBE','TrBR','C3G','C4G'};
+ageclasses={'1-10','11-20','21-30','31-40','41-50','51-60','61-70','71-80','81-90','91-100','101-110','111-120','121-130','131-140','>140'};
 
 if (length(dtype)~=nsims || length(dreal)~=nsims)
     error('Arrays describing simulations are not consistent in length')
@@ -49,6 +50,18 @@ for ss=1:nsims
     %Get LAI data
     lai=squeeze(lpj_to_grid_func_centre('lai_2001_2014',1,0));
     lai=lai(:,:,1:length(PFTs));
+    if ss==1
+        lai_1990=squeeze(lpj_to_grid_func_centre('lai_1961_1990',1,0));
+        lai_1990=lai_1990(:,:,1:length(PFTs));
+    end
+    
+    %Get disturbance probability data
+    distprob_in=squeeze(lpj_to_grid_func_centre('distprob_2001_2014',1,0));
+    distprob=distprob_in(:,:,1);
+    clear distprob_in
+    
+    %Get age class data
+    ageclass=squeeze(lpj_to_grid_func_centre('ageclass_2014',1,0));
     
     %Write each dataset to netcdf file
     
@@ -115,7 +128,46 @@ for ss=1:nsims
     note1='This data is the mean over the period 2001-2014, the year variable in this file is nominal';
     
     write_netcdf_lpjg(lai,variable,modname,disttype,realisation,year,...
-        units,variable_longname,output_dir,note1,PFTs);
+        units,variable_longname,output_dir,note1,PFTs,'PFT');
     
+    if ss==1
+        variable='LAI';
+        variable_longname='Leaf area index';
+        units='m2 (leaf) m-2 (ground)';
+        disttype=dtype{ss};
+        realisation=dreal{ss};
+        modname='LPJ-GUESS';
+        year=1990;
+        note1='This data is the mean over the period 1961-1990, the year variable in this file is nominal';
+        
+        write_netcdf_lpjg(lai_1990,variable,modname,disttype,realisation,year,...
+            units,variable_longname,output_dir,note1,PFTs,'PFT');
+    end
+    
+    %Disturbance probability
+    variable='distprob';
+    variable_longname='Disturbance probability';
+    units='fractional probability';
+    disttype=dtype{ss};
+    realisation=dreal{ss};
+    modname='LPJ-GUESS';
+    year=2014;
+    note1='This data is the mean over the period 2001-2014, the year variable in this file is nominal';
+    
+    write_netcdf_lpjg(distprob,variable,modname,disttype,realisation,year,...
+        units,variable_longname,output_dir,note1);
+    
+    %Age class fraction
+    variable='age';
+    variable_longname='Stand age';
+    units='fraction of grid-cell area';
+    disttype=dtype{ss};
+    realisation=dreal{ss};
+    modname='LPJ-GUESS';
+    year=2014;
+    note1='This data is for the year 2014';
+    
+    write_netcdf_lpjg(ageclass,variable,modname,disttype,realisation,year,...
+        units,variable_longname,output_dir,note1,ageclasses,'AgeClass');
 end
 clear ss
--- lpjg_processing/write_netcdf_lpjg.m
+++ lpjg_processing/write_netcdf_lpjg.m
@@ -1,5 +1,5 @@
 function write_netcdf_lpjg(data,variable,modname,disttype,realisation,year,...
-            units,variable_longname,output_dir,note1,PFTs)
+            units,variable_longname,output_dir,note1,dim3labels,dim3name)
 %Write out to netcdf file. This function to be called from lpjg_to_netcdfs.m
 %
 %T. Pugh
@@ -10,12 +10,12 @@ fprintf('Creating output file\n')
 datadim=size(data);
 ndim=length(datadim);
 if ndim==3
-    npfts=datadim(3);
-    if length(PFTs)~=npfts
+    ndim3=datadim(3);
+    if length(dim3labels)~=ndim3
         error('Number of PFTs in files does not correspond to length of PFT name array')
     end
 else
-    npfts=1;
+    ndim3=1;
 end
 
 minlat=-89.75;
@@ -29,7 +29,7 @@ longrid=minlon:gridspace:maxlon;
 nlat=length(latgrid);
 nlon=length(longrid);
 
-fname=[variable,'_',modname,'_',realisation,'_',disttype,'.nc'];
+fname=[variable,'_',modname,'_',realisation,'_',disttype,'_',mat2str(year),'.nc'];
 outfile=[output_dir,'/',fname];
     
 nccreate(outfile,'latitude','Dimensions',{'latitude',nlat})
@@ -39,10 +39,10 @@ nccreate(outfile,'longitude','Dimensions',{'longitude',nlon})
 ncwrite(outfile,'longitude',longrid)
 ncwriteatt(outfile,'longitude','units','degrees_east');
 if ndim==3
-    nccreate(outfile,'PFT','Dimensions',{'PFT',npfts})
-    ncwrite(outfile,'PFT',1:npfts)
-    for nn=1:npfts
-        ncwriteatt(outfile,'PFT',mat2str(nn),PFTs{nn});
+    nccreate(outfile,dim3name,'Dimensions',{dim3name,ndim3})
+    ncwrite(outfile,dim3name,1:ndim3)
+    for nn=1:ndim3
+        ncwriteatt(outfile,dim3name,mat2str(nn),dim3labels{nn});
     end
 end
 nccreate(outfile,'time','Dimensions',{'time',Inf})
@@ -50,7 +50,7 @@ ncwrite(outfile,'time',year)
 ncwriteatt(outfile,'time','units','years');
 
 if ndim==3
-    nccreate(outfile,variable,'Dimensions',{'longitude','latitude','PFT','time'},'DeflateLevel',9)
+    nccreate(outfile,variable,'Dimensions',{'longitude','latitude',dim3name,'time'},'DeflateLevel',9)
     ncwrite(outfile,variable,permute(data,[2 1 3]))
 else
     nccreate(outfile,variable,'Dimensions',{'longitude','latitude','time'},'DeflateLevel',9)
